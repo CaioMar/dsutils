f5990ebb706278bcab5a04461f46c70f7d224c85
CaioMar <caio.outmatched@gmail.com>
2019-09-28 16:58:04 -0300

    Creating builder facets for query builder

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
+import querybuilder as qb
 import json
 
-# with open('./config.json','w') as file:
-#     json.dump(fp=file, obj=conf, indent=4)
+modelos = \
+{"123456":{
+"tabela1_{}":{
+"MAIN":True,
+"ALIAS":"tb1_{}",
+"DATABASE":"db",
+"VARS":{
+"var1_{}":{"SAFRAS":[0,1],"DEFASAGEM":0},
+"var2_{}":{"SAFRAS":[0],"DEFASAGEM":0}
+}
+},
+"tabela2_{}":{
+"ALIAS":"tb2_{}",
+"DATABASE":"db",
+"VARS":{
+"var3_{}":{"SAFRAS":[0],"DEFASAGEM":1},
+"var4_{}":{"SAFRAS":[0,3],"DEFASAGEM":0}
+}
+}
+}}
 
-with open('./config.json','rb') as file:
-    config2 = json.load(fp=file)
+
+with open('./config.json','w') as file:
+    json.dump(fp=file, obj=modelos, indent=4)
 
 
-
-print(1)
-
-
-print(config2['modelo1'].keys())
+# with open('./config.json','rb') as file:
+#     config2 = json.load(fp=file)
+#
+# class SelectBuilder:
+#     """docstring for SelectBuilder."""
+#
+#     def __init__(self):
+#         self.
+#
+# print(config2['modelo1'].keys())
--- querybuilder.py
+++ querybuilder.py
@@ -1,98 +1,146 @@
-class QueryBuilder:
-    def __init__(self, database, table_name, table_alias, table_safra, table_key='nr_cpf_cnpj'):
-        self.__root = SelectQuery(database,
-                                  table_name,
-                                  table_alias,
-                                  table_safra,
-                                  table_key)
-
-    def left_join(self, database, table_name, table_alias, table_safra, table_key='nr_cpf_cnpj'):
-        self.__root.join_list.append(Table(database,
-                                                     table_name,
-                                                     table_alias,
-                                                     table_safra,
-                                                     table_key)
-                                               )
-        return self
+class Query:
+    def __init__(self):
+        self.main_table = None
+        self.join_list = []
+        self._all_tables = []
+        self._flag_var_alias = True
 
     def __str__(self):
-        return str(self.__root)
+        return ''
+        # if len(self.join_list)>0:
+        #     return f'select * from\n\t{self.main_table.database}.{self.main_table.table_name}_{self.main_table.table_safra} {self.main_table.table_alias}' + \
+        #             ''.join(['\nleft join\n\t{db}.{tb}_{safra} {alias}\n\ton {main_table_alias}.{main_table_key}={alias}.{key}'.format(
+        #                 db=tb.database,
+        #                 tb=tb.table_name,
+        #                 safra = tb.table_safra,
+        #                 alias=tb.table_alias,
+        #                 main_table_alias=self.main_table.table_alias,
+        #                 main_table_key=self.main_table.table_key,
+        #                 key=tb.table_key) for tb in self.join_list
+        #                 ])
+        # else:
+        #     return f'select * from {self.main_table.database}.{self.main_table.table_name}{self.main_table.table_safra}{self.main_table.table_alias}'
 
 
-class SelectQuery:
-    def __init__(self, database, table_name, table_alias, table_safra, table_key='nr_cpf_cnpj'):
+class QueryBuilder:
+    """docstring for QueryBuilder."""
+
+    def __init__(self, query=Query()):
+        self.query = query
+
+    @property
+    def left_join(self):
+        return QueryLeftJoinBuilder(self.query)
+
+    @property
+    def main_table(self):
+        return QueryMainTableBuilder(self.query)
+
+    def builder(self):
+        return self.query
+
+
+class QueryLeftJoinBuilder(QueryBuilder):
+    def __init__(self, query):
+        super().__init__(query)
+
+    def left_join(self, database, table_name, table_alias, table_safra, table_key='nr_cpf_cnpj'):
+        self.join_list.append(Table(database,
+                                    table_name,
+                                    table_alias,
+                                    table_safra,
+                                    table_key)
+                                    )
+        return self
+
+
+class QueryMainTableBuilder(QueryBuilder):
+    """docstring for QueryMainTableBuilder."""
+
+    def __init__(self, query):
+        super().__init__(query)
+
+    def main_table(self, database, table_name, table_alias, table_safra, table_key='nr_cpf_cnpj'):
         self.main_table = Table(database,
                                 table_name,
                                 table_alias,
                                 table_safra,
                                 table_key)
-        self.join_list = []
-
-    def __str__(self):
-        if len(self.join_list)>0:
-            return f'select * from\n\t{self.main_table.database}.{self.main_table.table_name}_{self.main_table.table_safra} {self.main_table.table_alias}' + \
-                    ''.join(['\nleft join\n\t{db}.{tb}_{safra} {alias}\n\ton {main_table_alias}.{main_table_key}={alias}.{key}'.format(
-                        db=tb.database,
-                        tb=tb.table_name,
-                        safra = tb.table_safra,
-                        alias=tb.table_alias,
-                        main_table_alias=self.main_table.table_alias,
-                        main_table_key=self.main_table.table_key,
-                        key=tb.table_key) for tb in self.join_list
-                        ])
-        else:
-            return f'select * from {self.main_table.database}.{self.main_table.table_name}{self.main_table.table_safra}{self.main_table.table_alias}'
+        return self
 
 
-    @staticmethod
-    def create(database, table_name, table_alias, table_safra, table_key='nr_cpf_cnpj'):
-        return QueryBuilder(database,
-                            table_name,
-                            table_alias,
-                            table_safra,
-                            table_key
-                )
 
-class Singleton(type):
-    """docstring for Singleton."""
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls)\
-            .__call__(*args,**kwargs)
-        return cls._instances[cls]
 
 
 class Table:
-    created_objects = []
 
     def __init__(self, database, table_name, table_alias, table_safra, table_key='nr_cpf_cnpj'):
-        if self.unique(table_alias, table_name+table_safra):
-            self.database = database
-            self.table_name = table_name
-            self.table_alias = table_alias
-            self.table_safra = table_safra
-            self.table_key = table_key
-            self.created_objects.append(self)
-        else:
-            raise AttributeError('Either table name or alias is not unique.')
+        self.database = database
+        self.table_name = table_name
+        self.table_alias = table_alias
+        self.table_safra = table_safra
+        self.table_key = table_key
+        self.variables = []
 
-    def unique(self, alias, tb_complete_name):
-        for tb in self.created_objects:
-            if alias == tb.table_alias:
-                return False
-            if tb_complete_name == tb.table_name + tb.table_safra:
-                return False
-        return True
+    def add_var(self, var_name):
+        self.variables.append(var_name)
+        return self
 
-    def __str__(self):
-        return self.database + ' ' + self.table_alias + ' ' + self.table_safra
-
-
-d1 = Table('database','table1','a','201907')
-d2 = Table('database','table1','b','201908')
+if __name__ == '__main__':
+    d1 = Table.create_table('database','table1_{}','a_{}','201907').add_var('variavel1').add_var('variavel2')
+    d2 = Table.create_table('database','table2_{}','b_{}','201907').add_var('variavel3')
 # query = SelectQuery.create('database','table1','a','201907')\
 #         .left_join('database','table2','b','201907')
 
-print(d1, d2, d1==d2)
+    print(d1, d2, d1==d2)
+
+# from enum import Enum, auto
+    # @staticmethod
+    # def create(database, table_name, table_alias, table_safra, table_key='nr_cpf_cnpj'):
+    #     return QueryBuilder(database,
+    #                         table_name,
+    #                         table_alias,
+    #                         table_safra,
+    #                         table_key
+    #             )
+# class Singleton(type):
+#     """docstring for Singleton."""
+#     _instances = {}
+#
+#     def __call__(cls, *args, **kwargs):
+#         if cls not in cls._instances:
+#             cls._instances[cls] = super(Singleton, cls)\
+#             .__call__(*args,**kwargs)
+#         return cls._instances[cls]
+#
+# class TableSwitch(Enum):
+#     """docstring for TableSwitch."""
+#     VALID = auto()
+#     INVALID = auto()
+#
+# class TableTriggers(Enum):
+#     TABLE_NAME_CHECK = auto()
+#     TABLE_ALIAS_CHECK = auto()
+
+
+
+    # @staticmethod
+    # def create_table(database, table_name, table_alias, table_safra, table_key='nr_cpf_cnpj'):
+    #     # print(1)
+    #     # print(self.__state)
+    #     return Table(database,
+    #                  table_name,
+    #                  table_alias,
+    #                  table_safra,
+    #                  table_key)
+
+    # def _unique(self, alias, tb_complete_name):
+    #     for tb in self.__created_objects:
+    #         if alias == tb.table_alias.format(tb.table_safra): return False
+    #         if tb_complete_name == tb.table_name.format(tb.table_safra): return False
+    #     return True
+    #
+    # def _get_table(self, tb_complete_name):
+    #     for tb in self.__created_objects:
+    #         if tb_complete_name == tb.table_name.format(tb.table_safra): return tb
+    #     raise ValueError('Table doesn\'t exist.')
