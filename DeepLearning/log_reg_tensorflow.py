241b2a10dca019b535f653ff0a0113b3d9ceed25
Caio Martins <caio.outmatched@gmail.com>
2020-03-05 23:13:10 -0300

    MNIST with tensorflow logreg

--- DeepLearning/log_reg_tensorflow.py
+++ DeepLearning/log_reg_tensorflow.py
@@ -19,8 +19,8 @@ class MulticlassLogisticRegression:
         Caio Martins
     """
 
-    def __init__(self, learning_rate=1e-3, num_epochs=10000, verbose=True, print_epoch=1000, eps=1e-5,
-                 gd_type='full', batch_number = 10, mu = 0):
+    def __init__(self, learning_rate=1e-3, num_epochs=10000, verbose=True, print_epoch=1000, eps=1e-10,
+                 gd_type='full', batch_number = 10, mu = 0, decay = 0.9):
         self.learning_rate = learning_rate
         self.num_epochs = num_epochs
         self.verbose = verbose
@@ -29,24 +29,29 @@ class MulticlassLogisticRegression:
         self.gd_type = gd_type
         self.batch_number = batch_number
         self.mu = mu
+        self.decay = decay
 
     def score(self, y, y_pred):
+        
         accuracy = (y == y_pred).sum()/y.shape[0]
         return accuracy
 
     def _get_dims(self, X, y):
+
         self.K = np.unique(y).size
         self.N, self.D = X.shape
 
     def _initialize_weights(self):
+
         self.W = np.random.randn(self.D,self.K) / np.sqrt(self.D)
         self.b = np.zeros(self.K)
 
     def fit(self, X, y=None):
+
         self._get_dims(X, y)
         self._initialize_weights()
 
-        T = np.array([[0 if y[j] != i else 1 for i in range(self.K)] for j in range(self.N)]).astype(np.float32)
+        T = np.array([[0 if y[j] != i else 1 for i in range(self.K)] for j in range(self.N)])
 
         inputs = tf.placeholder(tf.float32, shape=[None,self.D], name='inputs')
         labels = tf.placeholder(tf.float32, shape=[None,self.K], name='labels')
@@ -56,9 +61,14 @@ class MulticlassLogisticRegression:
 
         Z = tf.matmul(inputs, W) + b #logits
 
+        saver = tf.train.Saver()
+
         cost = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits_v2(logits=Z, labels=labels))
 
-        train_op = tf.train.GradientDescentOptimizer(self.learning_rate, name='train').minimize(cost)
+        train_op = tf.train.RMSPropOptimizer(self.learning_rate,
+                                             decay=self.decay,
+                                             momentum=self.mu,
+                                             epsilon=self.eps).minimize(cost)
         self.pred_op = tf.arg_max(Z, 1)
 
 
@@ -73,11 +83,12 @@ class MulticlassLogisticRegression:
                 session.run(init)
 
                 for epoch in range(self.num_epochs):
-                    print("epoch: %d" % epoch, X.shape)
-                    session.run('train', feed_dict = {'inputs:0': X.reshape((-1,self.D)), 'labels:0': T.reshape((-1,self.K))})
-                    y_pred = session.run(self.pred_op, feed_dict = {"inputs:0": X.reshape((-1,self.D))})
 
-                    self.costs.append(cost.eval())
+                    session.run(train_op, feed_dict = {'inputs:0': X, 'labels:0': T})
+                    y_pred = session.run(self.pred_op, feed_dict = {"inputs:0": X})
+                    cost_val = session.run(cost, feed_dict = {"inputs:0": X, 'labels:0': T})    
+                    
+                    self.costs.append(cost_val)
                     self.clf_rates.append(self.score(y, y_pred))
                     
                     if epoch % self.print_epoch == 0:
@@ -85,6 +96,8 @@ class MulticlassLogisticRegression:
                             print("epoch: ", epoch,
                                   "cost: ", self.costs[-1],
                                   "accuracy: ", self.clf_rates[-1])
+                
+                saver.save(session, 'logreg_model',global_step=self.num_epochs)
 
         # elif self.gd_type == 'batch':
             
@@ -124,11 +137,11 @@ class MulticlassLogisticRegression:
         #                 self.best_b = self.b            
 
 
-    # def predict(self, X):
-    #     init = tf.global_variables_initializer()
-    #     with tf.Session as session:
-    #         session.run(init)
-    #         y_pred = session.run(self.pred_op, feed_dict={"X":X})
-    #     return y_pred
+    def predict(self, X):
+        with tf.Session() as session:
+            new_saver = tf.train.import_meta_graph('logreg_model-{}.meta'.format(self.num_epochs))
+            new_saver.restore(session, tf.train.latest_checkpoint('./'))
+            y_pred = session.run(self.pred_op, feed_dict={'inputs:0':X})
+        return y_pred
 
         
